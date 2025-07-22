72891afe418b04aead2ec56d6a8a15f1c56193f8
nuran61 <149605159+nuran61@users.noreply.github.com>
2025-07-22 09:17:44 +0300

    first commit

---
+++ resources/views/hakkimizda.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>hakkımızda</title>
+</head>
+<body>
+	<h1>hakkımızda sayfası controller dan gelir grup</h1> 
+	 <a href="{{ route('ilet.yon') }}">İletişim</a>
+
+</body>
+</html>
---
+++ resources/views/iletisim.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>iletişim</title>
+</head>
+<body>
+<h1>iletişim sayfası controllerdan gelir grup</h1>
+<a href="{{ route('hak.yon') }}">Hakkımızda</a>
+
+</body>
+</html>

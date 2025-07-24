c35aede8904cf3eef9b701e287ad4b6ec719c7e8
nuran61 <149605159+nuran61@users.noreply.github.com>
2025-07-24 18:07:56 +0300

    first commit

---
+++ public/js/homepage.js
@@ -0,0 +1,12 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", ()=> {
+    // Toggle mobile menu visibality
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", ()=> menuOpenButton.click
+());
+
--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -7,6 +7,8 @@
     <!--Linking Font Awesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="{{asset('style/css/homepage.css')}}">
+
+
 </head>
 <body>
 <!-- Header / Navbar -->
@@ -66,6 +68,75 @@
            </div>
        </section>
 
+       <!-- About secction -->
+       <section class="about-section">
+           <div class="section-content">
+               <div class="about-image-wrapper">
+                   <img src="img/About-Barista.jpeg" alt="About" class="about-image">
+               </div>
+               <div class="about-details">
+                   <h2 class="section-title">About Us</h2>
+                   <p class="text">At Coffee House in Berndorf, Germany, we pride ourselves on being a go-to destination for coffee lovers and conversatşon seekers alike. We're dedicated to providing an expectional coffee experience in a cozy and inviting atmosphere, where guests can relax, unwind, and enjoy their time in comfort.
+                   </p>
+                   <div class="social-link-list">
+                       <a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a>
+                       <a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a>
+                       <a href="#" class="social-link"><i class="fa-brands fa-twitter"></i></a>
+                   </div>
+               </div>
+           </div>
+       </section>
+
+       <!--Menu section -->
+       <section class="Menu section">
+           <div class="section-content">
+               <ul class="menu-list">
+                   <li class=menu-item>
+                       <img src="images/hot-beverages.png" alt="Hot Beverages" class="menu-image"/>
+                       <div class="menu-details">
+                           <h3 class="name">Hot Beverages</h3>
+                           <p class="text">Wide range of Steaming hot coffee to make you fresh and light.</p>
+                       </div>
+                   </li>
+                   <li class=menu-item>
+                       <img src="images/cold-beverages.png" alt="Cold Beverages" class="menu-image">
+                       <div class="menu-details">
+                           <h3 class="name">Cold Beverages</h3>
+                           <p class="text">Creamy and frothy cold coffee to make you cool.</p>
+                       </div>
+                   </li
+                   <li class=menu-item>
+                       <img src="images/refreshment.png" alt="Refreshment" class="menu-image">
+                       <div class="menu-details">
+                           <h3 class="name">Refreshment</h3>
+                           <p class="text">Fruit and icy refreshing drink to make feel refresh.</p>
+                       </div>
+                   </li
+                   <li class=menu-item>
+                       <img src="images/special-commbo.png" alt="Special Combos" class="menu-image">
+                       <div class="menu-details">
+                           <h3 class="name">Special Combos</h3>
+                           <p class="text">Your favorite eating and drinking combations.</p>
+                       </div>
+                   </li
+                   <li class=menu-item>
+                       <img src="images/desserts.png" alt="Desserts" class="menu-image">
+                       <div class="menu-details">
+                           <h3 class="name">Desserts</h3>
+                           <p class="text">Satite your palate and take you on a culinary treat.</p>
+                       </div>
+                   </li
+                   <li class=menu-item>
+                       <img src="images/burger-frenchfries.png" alt="Burger & French Fries" class="menu-image">
+                       <div class="menu-details"> <h3 class="name">Hot Beverages</h3>
+                           <p class="text">Quick bites to satisfy your small size hunger.</p>
+                       </div>
+                   </li
+               </ul>
+           </div>
+       </section>
    </main>
+
+<script src="{{asset('js/homepage.js')}}"></script>
 </body>
 </html>
